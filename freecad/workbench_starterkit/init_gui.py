02086b2265d7dffbfa6e02504d6d0dc9937017e9
Joel Graff <monograff76@gmail.com>
2019-09-05 07:24:45 -0500

    documentation

--- freecad/workbench_starterkit/init_gui.py
+++ freecad/workbench_starterkit/init_gui.py
@@ -1,18 +1,112 @@
+#/**********************************************************************
+#*                                                                     *
+#* Copyright (c) XXXX FreeCAD AUthor <freecad_author@gmail.com>               *
+#*                                                                     *
+#* This program is free software; you can redistribute it and/or modify*
+#* it under the terms of the GNU Lesser General Public License (LGPL)  *
+#* as published by the Free Software Foundation; either version 2 of   *
+#* the License, or (at your option) any later version.                 *
+#* for detail see the LICENCE text file.                               *
+#*                                                                     *
+#* This program is distributed in the hope that it will be useful,     *
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+#* GNU Library General Public License for more details.                *
+#*                                                                     *
+#* You should have received a copy of the GNU Library General Public   *
+#* License along with this program; if not, write to the Free Software *
+#* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307*
+#* USA                                                                 *
+#*                                                                     *
+#***********************************************************************
+"""
+GUI Initialization module
+"""
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  First Things First
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  Here at the top of the module, it's good practice to always
+#  the license text.  Add your name and email.  Date is optional
+#  and reflects the date of first publication.
+#
+#  Per standard Python practice, imports are located at the top of the
+#  file, with the exception of workbench commands - more later on that.
+#
+#  Also note the use of the TEMPLATEWB_VERSION constant.  This provides
+#  a quick, easy way to version your workbench.  It's also indicated
+#  in the FreeCAD UI for users' convenience
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 import os
 import FreeCADGui as Gui
 import FreeCAD as App
 from freecad.workbench_starterkit import ICONPATH
 
+#use this to track workbench versioning
+TEMPLATEWB_VERSION = '(alpha)'
 
 class template_workbench(Gui.Workbench):
     """
     class which gets initiated at starup of the gui
     """
 
-    MenuText = "template workbench"
-    ToolTip = "a simple template workbench"
+    #Constants for UI locations for toolboxes
+    MENU = 1
+    TOOLBAR = 2
+    CONTEXT = 4
+
+    #Workbench GUI-specific attributes
+    MenuText = "Template Workbench" + TEMPLATEWB_VERSION
+    ToolTip = "An example template workbench"
     Icon = os.path.join(ICONPATH, "template_resource.svg")
     toolbox = []
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# The Command Dictionary
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This dictionary provides a straight-forward, easy way to 
+# manage workbench commands.  It contains a list of all command
+# toolboxes and the locations in the UI where they appear.
+#
+# The top-level items are dictionaries which represent a toolbox
+# to which commands may be assigned.  
+# 
+# The 'gui' value determines the UI locations in which a
+# toolbox appears.
+#
+# The 'cmd' list contains the names of all commands which are
+# assigned to that toolbox.  Commands may be assigned to
+# multiple toolboxes.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        self.command_ui = {
+
+            'Toolbox 1': {
+                'gui': self.menu + self.toolbar,
+                'cmd': ['MyCommand1']
+            },
+
+            'Toolbox 2': {
+                'gui': self.menu + self.toolbar + self.context,
+                'cmd': ['MyCommand2', 'MyCommand3']
+            },
+
+            'Toolbox 3': {
+                'gui': self.menu + self.toolbar + self.context,
+                'cmd': ['MyCOmmand1', 'MyCommand3']
+            },
+        }
 
     def GetClassName(self):
         return "Gui::PythonWorkbench"
@@ -20,14 +114,49 @@ class template_workbench(Gui.Workbench):
     def Initialize(self):
         """
         This function is called at the first activation of the workbench.
-        here is the place to import all the commands
+        Import commands here
         """
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  Starting Up
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  When the workbench is first activated (the user explicitly attempts
+#  to load it), the commands are loaded.
+#
+#  Placing imports here, rather than at the top of the file, defers
+#  additional loading when FreeCAD starts, as all workbench classes are
+#  constructed then.
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        #import commands here to be added to the user interface
         from freecad.workbench_starterkit import my_numpy_function
-        App.Console.PrintMessage("switching to workbench_starterkit")
-        App.Console.PrintMessage("run a numpy function: sqrt(100) = {}".format(my_numpy_function.my_foo(100)))
+
+        from .commands import my_command_1, my_command_2, my_command_3
+
+        #iterate the command toolboxes defined in __init__() and add
+        #them to the UI according to the assigned location flags
+        for _k, _v in self.command_ui.items():
+
+            if _v['gui'] & self.toolbar:
+                self.appendToolbar(_k, _v['cmd'])
+
+            if _v['gui'] & self.menu:
+                self.appendMenu(_k, _v['cmd'])
 
         self.appendToolbar("Tools", self.toolbox)
         self.appendMenu("Tools", self.toolbox)
+
+        #Feel free to add diagnostic code or other start-up related
+        #activities...
+        App.Console.PrintMessage("Switching to workbench_starterkit")
+
+        App.Console.PrintMessage(
+            "run a numpy function: sqrt(100) = {}".format(my_numpy_function.my_foo(100))
+        )
 
     def Activated(self):
         '''
@@ -41,5 +170,14 @@ class template_workbench(Gui.Workbench):
         '''
         pass
 
+    def ContextMenu(self, recipient):
+        """
+        Right-click menu options
+        """
+
+        #Populate the context menu when it's called
+        for _k, _v in self.fn.items():
+            if _v['gui'] & self.context:
+                self.appendContextMenu(_k, _v['cmds'])
 
 Gui.addWorkbench(template_workbench())
